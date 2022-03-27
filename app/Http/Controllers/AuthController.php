f6a34e02a11e5a937ca43cff05a0d1bbc2017f5d
Losemanki6656 <bloggerg420@gmail.com>
2022-03-27 17:53:46 +0500

    fix: composer.json

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,7 +31,7 @@ class AuthController extends Controller
             return response()->json($validator->errors(), 422);
         }
         if (! $token = auth('api')->attempt($validator->validated())) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'Unauthorized'], 422);
         }
         return $this->createNewToken($token);
     }
