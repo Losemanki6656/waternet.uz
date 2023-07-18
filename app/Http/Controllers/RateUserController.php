50b4a1dd66764f90f00d08a53158c565095becf6
Losemanki <bloggerg420@gmail.com>
2023-07-18 14:38:22 +0500

    asd

--- app/Http/Controllers/RateUserController.php
+++ app/Http/Controllers/RateUserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Client;
 use App\Models\ClientChat;
 use App\Models\RateUser;
+use App\Models\Sms;
 use App\Models\SuccessOrders;
 use Illuminate\Http\Request;
 
@@ -14,31 +15,62 @@ class RateUserController extends Controller
     public function index()
     {
         $rates = RateUser::where('client_id', request('client_id',0))
-            ->where('status', false)
-            ->get();
+            ->where('status', false);
 
-        $arr = [];
-        foreach ($rates as $item) {
-            $tg = ClientChat::where('client_id', request('client_id'))->first();
-            if($tg) {
+        $tg = ClientChat::where('client_id', request('client_id'))->first();
 
-                $order = SuccessOrders::find($item->success_order_id);
-    
-                $text = "Получено - ".$order->amount.", Доставлено - ".$order->count.", Возврат тар - ".
-                $order->container.", Предоплата ".Client::find($order->client_id)->balance.". Спасибо за покупки!";
+        $newMessages = [];
+        if($rates->count())
+        {
+            $arr = [];
+            foreach ($rates->get() as $item) {
+                if($tg) {
 
-                $arr[] = [
-                    'id' => $item->id,
-                    'message' => $text,
-                    'rate_msg' => 'Доставшик хизматини бахоланг ...',
-                    'chat_id' => $tg->chat_id
-                ];
+                    $order = SuccessOrders::find($item->success_order_id);
+        
+                    $text = "Получено - ".$order->amount.", Доставлено - ".$order->count.", Возврат тар - ".
+                    $order->container.", Предоплата ".Client::find($order->client_id)->balance.". Спасибо за покупки!";
+
+                    $arr[] = [
+                        'id' => $item->id,
+                        'message' => $text,
+                        'rate_msg' => 'Доставшик хизматини бахоланг ...',
+                        'chat_id' => $tg->chat_id
+                    ];
+                }
+
             }
 
+            $newMessages[] = [
+                'type' => 'rate',
+                'data' => $arr
+            ];
         }
 
+        $sms = Sms::where('client_id', request('client_id',0));
+
+        if($sms->count()) {
+            $arr = [];
+            foreach ($sms->get() as $item) {
+                if($tg) {
+
+                    $arr[] = [
+                        'message' => 'message',
+                        'photo' => 'https://yobte.ru/uploads/posts/2019-11/devushki-v-lesu-75-foto-62.jpg'
+                    ];
+                }
+
+            }
+
+            $newMessages[] = [
+                'type' => 'newMessage',
+                'data' => $arr
+            ];
+        }
+       
+
         return response()->json([
-            'data' => $arr
+            'messages' => $newMessages
         ]);
     } 
 
