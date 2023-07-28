23bde271f126c3103bb0ba9a156d00dd5a0812f6
Losemanki6656 <bbiznestg@gmail.com>
2023-07-28 15:17:54 +0500

    Asd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1721,17 +1721,21 @@ class HomeController extends Controller
                     )
                 );
                 $keyboard = json_encode($keyboard);
-                $text = "Получено - " . $successorder->amount . ", Доставлено - " . $successorder->count . ", Возврат тар - " .
+                $text = "Спасибо за покупки! <br> Получено - " . $successorder->amount . ", Доставлено - " . $successorder->count . ", Возврат тар - " .
                     $successorder->container . ", Предоплата " . $client_info->balance;
 
                 Http::post('https://api.telegram.org/bot6325632109:AAFqHouzLr-OB_ODDvPiDeLN8RJmiNJAP0w/sendMessage', [
                     'chat_id' => $bot->chat_id,
                     'text' => $text,
+                    "parse_mode" => "HTML"
+                ]);
+                $rate_text = "Доставшик хизматини бахоланг ...";
+                Http::post('https://api.telegram.org/bot6325632109:AAFqHouzLr-OB_ODDvPiDeLN8RJmiNJAP0w/sendMessage', [
+                    'chat_id' => $bot->chat_id,
+                    'text' => $rate_text,
                     "parse_mode" => "HTML",
                     'reply_markup' => $keyboard
                 ]);
-
-                dd($text);
             }
 
 
