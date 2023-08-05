e35dca68c9c8fb0a33c46932592a9fe3ad7e7cd3
Losemanki6656 <bbiznestg@gmail.com>
2023-08-06 02:08:49 +0500

    Asd

--- database/migrations/2023_07_18_234239_change_column_to_orders_table.php
+++ database/migrations/2023_07_18_234239_change_column_to_orders_table.php
@@ -15,31 +15,31 @@ class ChangeColumnToOrdersTable extends Migration
     {
         Schema::table('orders', function (Blueprint $table) {
 
-            $table->BigInteger('organization_id')->nullable()->unsigned()->change();
-            $table->index('organization_id');
-            $table->foreign('organization_id')->references('id')->on('organizations');
+            // $table->BigInteger('organization_id')->nullable()->unsigned()->change();
+            // $table->index('organization_id');
+            // $table->foreign('organization_id')->references('id')->on('organizations');
 
-            $table->BigInteger('user_id')->nullable()->unsigned()->change();
-            $table->index('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            // $table->BigInteger('user_id')->nullable()->unsigned()->change();
+            // $table->index('user_id');
+            // $table->foreign('user_id')->references('id')->on('users');
 
-            $table->BigInteger('city_id')->nullable()->unsigned()->change();
-            $table->index('city_id');
-            $table->foreign('city_id')->references('id')->on('sities');
+            // $table->BigInteger('city_id')->nullable()->unsigned()->change();
+            // $table->index('city_id');
+            // $table->foreign('city_id')->references('id')->on('sities');
 
-            $table->BigInteger('area_id')->nullable()->unsigned()->change();
-            $table->index('area_id');
-            $table->foreign('area_id')->references('id')->on('areas');
+            // $table->BigInteger('area_id')->nullable()->unsigned()->change();
+            // $table->index('area_id');
+            // $table->foreign('area_id')->references('id')->on('areas');
 
-            $table->BigInteger('client_id')->nullable()->unsigned()->change();
-            $table->index('client_id');
+            // $table->BigInteger('client_id')->nullable()->unsigned()->change();
+            // $table->index('client_id');
             $table->foreign('client_id')->references('id')->on('clients');
 
-            $table->BigInteger('product_id')->nullable()->unsigned()->change();
+            // $table->BigInteger('product_id')->nullable()->unsigned()->change();
             $table->index('product_id');
             $table->foreign('product_id')->references('id')->on('products');
 
-            $table->boolean('type')->default(0)->after('status');
+            // $table->boolean('type')->default(0)->after('status');
         });
     }
 
@@ -51,7 +51,7 @@ class ChangeColumnToOrdersTable extends Migration
     public function down()
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->dropColumn("type");
+            // $table->dropColumn("type");
         });
     }
 }
